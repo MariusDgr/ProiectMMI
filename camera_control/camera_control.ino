0ddc21ff62f310b1ba3daa686078e19ca8a974bc
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2020-01-12 20:09:50 +0200

    wrote rest server

--- camera_control/camera_control.ino
+++ camera_control/camera_control.ino
@@ -1,10 +1,15 @@
 #include <Servo.h>
 
 void swapInt(int &a, int &b);
+void buzz_sound(int pin, int pitch, int time);
+void test_z_rot(int start, int low, int high);
 
 // Pinout 
 #define yAxisPin 5
 #define zAxisPin 6
+
+#define ledPin 10
+#define buzzPin 3 
 
 // Settings
 int zAxisLowLimit = 0;
@@ -16,6 +21,8 @@ bool invertedZ = false;
 bool invertedY = false;
 
 int alarmTime = 1000;
+int alarmPitch = 1000;
+int alarmEnabled = true;
 
 // Variables and objects
 Servo yAxisServo;
@@ -31,7 +38,7 @@ int angVal = 0;
 
 
 // States
-bool detected;
+bool intruderDetected = false;
 
 void setup() {
   // if(invertedY){
@@ -40,6 +47,8 @@ void setup() {
   // if(invertedZ){
   //   swapInt(zAxisHighLimit, zAxisLowLimit);
   // }
+
+  pinMode(ledPin, OUTPUT);
   
   yAxisServo.attach(yAxisPin, yAxisLowLimit, yAxisHighLimit);
   zAxisServo.attach(zAxisPin, zAxisLowLimit, zAxisHighLimit);
@@ -63,15 +72,32 @@ void loop() {
     Serial.print("Received: ");
     Serial.println(angVal);
 
-    // zAxisAngle = angVal;
-    yAxisAngle = angVal;
+    if(angVal == 1){
+      intruderDetected = true;
+    }
 
-    move_camera(yAxisAngle, zAxisAngle);
+    // zAxisAngle = angVal;
+    // yAxisAngle = angVal;
+
+    // move_camera(yAxisAngle, zAxisAngle);
   }
 
-  test_z_rot(zStartAng, zAxisHighLimit, zAxisLowLimit);
+  // test_z_rot(zStartAng, zAxisHighLimit, zAxisLowLimit);
 
+  if(alarmEnabled){
+    if(intruderDetected == 1){
+      digitalWrite(ledPin, HIGH);
 
+      // buzzer 
+      buzz_sound(buzzPin, alarmPitch, alarmTime);
+
+      intruderDetected = false;
+
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  
 }
 
 void move_camera(int yAngle, int zAngle){
@@ -102,4 +128,10 @@ void test_z_rot(int start, int low, int high){
     move_camera(i, zAxisAngle);
     delay(50);
   }
+}
+
+void buzz_sound(int pin, int pitch, int time){
+  tone(pin, pitch);
+  delay(time);
+  noTone(pin);
 }
---
+++ server/server.py
@@ -0,0 +1,97 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+liquids = [
+    {
+        "name": "water",
+        "volume": 2000,
+    },
+    {
+        "name": "cola",
+        "volume": 1000,
+    },
+    {
+        "name": "fanta",
+        "volume": 1000,
+    }
+]
+
+
+class Liquid(Resource):
+
+    def get(self, name):
+        for liquid in liquids:
+            if(name == liquid["name"]):
+                return liquid, 200
+        return "Liquid not found", 404
+ 
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("volume")
+        args = parser.parse_args()
+
+        for liquid in liquids:
+            if(name == liquid["name"]):
+                return "Liquid {} already exists",format(name), 400
+
+        liquid = {
+            "name": name,
+            "volume": 1000,
+        }
+
+        liquids.append(liquid)
+        return liquid, 201
+ 
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("volume")
+        args = parser.parse_args()
+
+        for liquid in liquids:
+            if(name == liquid["name"]):
+                liquid["volume"] = args["volume"]
+                return liquid, 200
+
+        liquid = {
+            "name": name,
+            "volume": 1000,
+        }
+
+        liquids.append(liquid)
+        return liquid, 201
+ 
+    def delete(self, name):
+        global liquids
+        liquids = [liquid for liquid in liquids if liquid["name"] != name]
+        return "Deleted {} liquid.".format("name"), 200
+
+
+api.add_resource(Liquid, "/liquid/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# tasks = [
+#     {
+#         'id': 1,
+#         'title': u'Buy groceries',
+#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'title': u'Learn Python',
+#         'description': u'Need to find a good Python tutorial on the web', 
+#         'done': False
+#     }
+# ]
+
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+# def get_tasks():
+#     return jsonify({'tasks': tasks})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
